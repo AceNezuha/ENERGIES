04f5c972a1a00153e79b3441b6101cae22e44019
Badrul Haikal Iman <100863856+AceNezuha@users.noreply.github.com>
2025-04-21 17:09:10 +0800

    Update db.js

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,16 +1,15 @@
+// backend/db.js
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  appName: 'RenewableEnergyPlatform'
+  appName: 'ENERGIES'
 })
   .then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
-// Optional: listen to events
 const db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('✅ MongoDB connection open');
